591e5b6b3ffd1da1ab2db005beb8ea1a0671d921
Артём <follarchi@rambler.ru>
2017-05-06 14:53:05 +0300

    add signin form in index

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Вход</title>
+</head>
+<body>
+    <header class="main-header">
+        <div class="container">
+
+    
+
+
+    </header>
+
+<section class="login">
+<div class="container">
+<h2 class="login-title">Вход</h2>
+<form action="" class="signin-form" method="post">
+    <table>
+        <tr>
+            <td class="title">
+                <label for="login">Логин:</label>
+            </td>
+            <td class="field">
+                <input type="text" id="login-field" name="login-field" placeholder="Введите логин">
+            </td>
+        </tr>
+        <tr>
+            <td class="title">
+                <label for="password">Пароль:</label>
+            </td>
+            <td class="field">
+                <input type="password" id="password-field" name="password-field" placeholder="Введите пароль">
+            </td>
+        </tr>
+        <tr>
+            <td class="field">
+                <input type="submit" class="btn" value="Войти">
+            </td>
+            <td class="field">
+                <input type="submit" class="btn" value="Зарегистрироваться">
+            </td>
+        </tr>
+    </table>
+</form>
+</div>
+</section>
+</body>
+</html>
